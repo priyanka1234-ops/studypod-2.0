0bb966e864c37fbebb04a8c733d76538f63af8e1
priyanka1234-ops <86639672+priyanka1234-ops@users.noreply.github.com>
2021-08-27 10:41:13 +0530

    Add files via upload

---
+++ numcheck.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std; 
+int main() { 
+    	int number; 	
+        cout<<"Enter The Number You Want To Check?"<<endl ; 	
+        cin>>number; 	
+        if(number%2==0) 
+        	{ 		cout<<"Number is Even"; 	} 	
+        else 	 
+        	{ 		cout<<"Number is ODD"; 	} 
+            
+            return 0 ;
+            }
